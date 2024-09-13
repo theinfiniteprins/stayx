9ad9a436e60a390b9906bc43ada7012a2dd3b51a
theinfiniteprins <prinsbhuva2004@gmail.com>
2024-09-13 20:30:48 +0530

    made slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,38 @@
+// src/components/Home.js
 import React from 'react';
+import Slider from 'react-slick';
+import '../styles.css';
+
+// Dummy data for the slider; in a real application, this would come from an API or state
+const propertyImages = [
+  '/images/property1.jpg',
+  '/images/property2.png',
+  '/images/property3.jpg',
+];
 
 const Home = () => {
+  // Slider settings
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,        // Enable autoplay
+    autoplaySpeed: 7000    // Set autoplay interval to 7 seconds (7000 milliseconds)
+  };
+
   return (
-    <div>
-      {/* More content for the homepage will go here */}
+    <div className="home-container">
+      <div className="slider-container">
+        <Slider {...settings}>
+          {propertyImages.map((image, index) => (
+            <div key={index} className="slider-item">
+              <img src={image} alt={`Property ${index + 1}`} className="slider-image" />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
